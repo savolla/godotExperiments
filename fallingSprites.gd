ed0247f6a78e47d1fde5f46dee3ea2bc9c740b24
Oleksiy <savolla@protonmail.com>
2019-02-08 01:12:51 +0300

    Update and rename fallingButtons.tscn to fallingSprites.gd

--- fallingSprites.gd
+++ fallingSprites.gd
@@ -1,8 +1,4 @@
-[gd_scene load_steps=2 format=2]
-
-[sub_resource type="GDScript" id=1]
-
-script/source = "########################################################################## 
+########################################################################## 
 # This script creates two nodes (Button and Sprite)
 # and when the Button is pressed it spawns a Sprite at a random
 # location then makes the sprite fall down. Then increments a number
@@ -23,19 +19,17 @@ func _ready(): # this function is a built-in function of Godot game engine. it's
 	
 	randomize() # sets a random seed (srand() like). we need it to spawn the sprite on random locations
 
-	# INITIALIZATION OF NODES
-
 	# initializing button object
 	self.add_child(buttonObject) # adding button object as a child of Node2D. self is the script owner node
-	buttonObject.text = \"Press Me\" # setting text to displaying on the button
+	buttonObject.text = "Press Me" # setting text to displaying on the button
 	buttonObject.rect_position = Vector2(200,200) # setting buttons position on the screen. Vector2(x,y)
 	
 	# when button is pressed make the sprite fall down. self is the spript owner node
-	buttonObject.connect(\"pressed\", self, \"userDefinedFallDownFunction\")
+	buttonObject.connect("pressed", self, "userDefinedFallDownFunction")
 
 func userDefinedFallDownFunction():
 	self.add_child(newlyCreatedSprite) # adding a sprite node to make it fall
-	newlyCreatedSprite.texture = preload(\"res://icon.png\") # setting the texture of this sprite
+	newlyCreatedSprite.texture = preload("res://icon.png") # setting the texture of this sprite
 	newlyCreatedSprite.position = Vector2( randi() % 500, randi() % 300 ) # spawn it on random locations
 	buttonObject.text = str(numberToDisplayOnButtonObject) # incrment the number on the button
 	numberToDisplayOnButtonObject += 1 # increment the number to display it later
@@ -43,25 +37,5 @@ func userDefinedFallDownFunction():
 	
 func _process(delta): # another built-in function. This function is called every frame of the game
 	newlyCreatedSprite.position.y += 30 # every frame increment Y coordinate of the sprite to make it fall
-	
-	
 
-
-	
-	
-	
-	
-	
-
-
-
-
-
-
-
-
-"
-
-[node name="Node2D" type="Node2D"]
-script = SubResource( 1 )
 
